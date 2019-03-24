a6a03271ff8eac2042b01cbad51880c3387415c7
ahsan <muhammad.ahsan@gmail.com>
2019-03-24 17:30:59 +0100

    Set up via API

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
+import connexion
+from flask import jsonify
+from connexion import RestyResolver
+
+connexion_app = connexion.FlaskApp(__name__, specification_dir='swagger/')
+app = connexion_app.app
+app.url_map.strict_slashes = False
+connexion_app.add_api('api.yaml', resolver=RestyResolver('api'))
+
+
+@app.route('/')
+def index_html():
+    return jsonify({'message': 'Started API successfully ...'})
+
+
 if __name__ == '__main__':
-    print('This is main')
+    print('Welcome from AI driven intellisense')
+    app.run(port=8080)
