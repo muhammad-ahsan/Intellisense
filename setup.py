76dfbc14c6884c027db1c8cf31a8a77bc56b2d29
ahsan <muhammad.ahsan@gmail.com>
2019-03-24 14:26:28 +0100

    Added setup.py skeleton

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='intellisense',
+    version='1.0',
+    packages=['intellisense', ],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
