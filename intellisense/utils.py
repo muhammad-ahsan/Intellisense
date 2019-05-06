0111cb7832aca6b8f548b5f62ead467d18b50397
Muhammad Ahsan <muhammad.ahsan@mytaxi.com>
2019-05-07 00:59:00 +0200

    Minor refactor

--- intellisense/utils.py
+++ intellisense/utils.py
@@ -4,12 +4,11 @@ from intellisense import prefix_tree
 
 
 def health_check():
-    return jsonify({'id': uuid.uuid1(), 'message': 'Health is good'})
+    return jsonify({'id': uuid.uuid1(), 'message': 'The service is in good health'})
 
 
 def response():
     query = request.args.get('query')
     print(query)
     result = prefix_tree.recommend(query)
-    print(result)
     return jsonify({'id': uuid.uuid1(), 'list': result})
