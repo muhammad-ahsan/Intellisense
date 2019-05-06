635e25c73c87b1e1aa3a79c54b8a2ce415493b9a
Muhammad Ahsan <muhammad.ahsan@mytaxi.com>
2019-05-07 00:10:44 +0200

    Bump version using semantic versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='intellisense',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     license='MIT License',
     long_description=open('README.md').read(),
