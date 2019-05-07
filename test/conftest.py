3ce01331897b36f831d3ace2349d14a29f85e603
Muhammad Ahsan <muhammad.ahsan@mytaxi.com>
2019-05-07 02:59:14 +0200

    Using a separate conftest.py for fixtures for project

---
+++ test/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from intellisense import prefix_tree
+
+
+@pytest.fixture
+def my_prefix_tree():
+    return prefix_tree
--- test/test_intellisense.py
+++ test/test_intellisense.py
@@ -1,12 +1,2 @@
-import pytest
-
-from intellisense import prefix_tree
-
-
-@pytest.fixture
-def my_prefix_tree():
-    return prefix_tree
-
-
 def test_prefix_tree(my_prefix_tree):
     assert len(my_prefix_tree.recommend('')) == 0
