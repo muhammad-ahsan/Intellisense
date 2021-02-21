36f1b0197359152b76178dc16c3b80069f48c510
Muhammad Ahsan <muhammad.ahsan@gmail.com>
2021-02-21 19:38:20 +0100

    Added test for algorithms.py

--- test/test_algorithms.py
+++ test/test_algorithms.py
@@ -1,3 +1,25 @@
-def test_prefix_tree():
-    # assert len(my_prefix_tree.recommend('')) == 0
-    pass
+"""Module to test various algorithms"""
+import pytest
+from intellisense.algorithms import PrefixTree, PhoneticIndex
+
+
+@pytest.fixture()
+def prefix_tree_obj() -> PrefixTree:
+    """Providing prefix tree object"""
+    return PrefixTree({"example", "ekzampul"})
+
+
+@pytest.fixture()
+def phonetic_index_obj() -> PhoneticIndex:
+    """Providing phonetic index object"""
+    return PhoneticIndex({"example", "ekzampul"})
+
+
+def test_prefix_tree(prefix_tree_obj):
+    """Testing prefix tree built with custom vocabulary"""
+    assert "example" in prefix_tree_obj.recommend("example")
+
+
+def test_phonetic_index(phonetic_index_obj):
+    """Testing phonetic index built with custom vocabulary"""
+    assert "example" in phonetic_index_obj.recommend("example")
