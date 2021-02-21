773041dd6d7ad6c4dcba5433ecf12505bf92fc4d
Muhammad Ahsan <muhammad.ahsan@gmail.com>
2021-02-21 13:10:15 +0100

    Minor refactoring

--- intellisense/utils.py
+++ intellisense/utils.py
@@ -1,14 +1,21 @@
+"""API call handler module"""
 import uuid
 from flask import jsonify, request
-from intellisense import prefix_tree
+import algorithms
+import helper
+
+prefix_tree = algorithms.PrefixTree(helper.get_vocabulary("en"))
+phonetic_index = algorithms.PhoneticIndex(helper.get_vocabulary("en"))
 
 
 def health_check():
+    """Checking health of api"""
     return jsonify({'id': uuid.uuid1(), 'message': 'The service is in good health'})
 
 
 def response():
+    """Returning response to api call"""
     query = request.args.get('query')
-    print(query)
-    result = prefix_tree.recommend(query)
+    result = set()
+    result = result.union(prefix_tree.recommend(query)).union(phonetic_index.recommend(query))
     return jsonify({'id': uuid.uuid1(), 'list': result})
