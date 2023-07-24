edda5e82471ce8da2aa4c3c85bf20af28edb0a9a
ahsan <muhammad.ahsan@gmail.com>
2023-07-24 08:29:04 +0200

    Update test_algorithms.py

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -17,7 +17,4 @@ def test_phonetic_index_recommend(phonetic_index):
 
 @pytest.mark.parametrize(('input_n', 'expected'), (("", 0), (None, 0)))
 def test_phonetic_index(phonetic_index, input_n, expected):
-    """Testing phonetic index built with custom vocabulary"""
-    print(f"{input_n} and {expected}")
-    assert "example" in phonetic_index.recommend("example")
     assert len(phonetic_index.recommend(input_n)) == expected
