c8b1fca88dfede36d07b3d649c8f486d4dc49c46
ahsan <muhammad.ahsan@gmail.com>
2023-07-24 18:23:04 +0200

    Cleanup main

--- main.py
+++ main.py
@@ -22,25 +22,3 @@ async def recommendation(keyword: str):
 def health_check():
     return {'id': uuid.uuid1(), 'message': 'The api is healthy'}
 
-#
-# options = {"swagger_ui": True,
-#            'swagger_url': '/api'}
-#
-# connexion_app = connexion.FlaskApp(__name__, specification_dir='swagger/', options=options)
-# app = connexion_app.app
-# connexion_app.add_api('api.yaml', resolver=RestyResolver('api'))
-#
-#
-# @app.route('/', methods=['POST', 'GET'])
-# def io_html():
-#     hints = []
-#     if request.method == 'POST':
-#         word = request.form['word']
-#         hints.append(get_recommendations_list(word))
-#         return render_template('io.html', hints=hints)
-#     else:
-#         return render_template('io.html')
-
-
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
