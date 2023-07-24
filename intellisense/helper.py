435f55070ac795073e10f56769da38782fbea521
ahsan <muhammad.ahsan@gmail.com>
2023-07-25 00:04:14 +0200

    added type hints

--- intellisense/helper.py
+++ intellisense/helper.py
@@ -1,13 +1,14 @@
 import io
 import os
 import zipfile
+from typing import Set
 
 
-def get_zipped_vocabulary(zip_path: str) -> set:
+def get_zipped_vocabulary(zip_path: str) -> Set[str]:
     exists = os.path.isfile(zip_path)
     if not exists:
         raise FileNotFoundError('Zipped vocabulary don\'t exist')
-    words = set()
+    words: Set[str] = set()
     with zipfile.ZipFile(zip_path) as compressed_file:
         files = compressed_file.namelist()
         for file_name in files:
@@ -17,7 +18,7 @@ def get_zipped_vocabulary(zip_path: str) -> set:
     return set(map(lambda x: x.lower(), words))
 
 
-def get_vocabulary(language_iso_code: str) -> set:
+def get_vocabulary(language_iso_code: str) -> Set[str]:
     if language_iso_code not in ["en", "de"]:
         raise ValueError
     source_dir = os.path.dirname(os.path.abspath(__file__)) + '/data/'
